c970cac95a4e761d9764b2ee017741dd1e6361e2
shravan20 <mrshravankumarb@gmail.com>
2021-04-04 11:14:36 +0530

    [ Added APIcall in Utils with dependency ] feat | Added ApiFetch-Call Dependency

---
+++ google-sheets-rest-api/src/utils/apiCall.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+
+
+exports.apiFetch = async (url, body, method) => {
+
+    return await fetch(url, { 
+        method: method, 
+        body: body 
+    });
+
+};
